2b29a91f4915bb2997a7562b2aabea08c299edee
Chris Tralie <chris.tralie@gmail.com>
2020-12-11 13:22:56 -0500

    Setting up skeleton code

--- FABRIK.js
+++ FABRIK.js
@@ -1,29 +1,19 @@
 let vec3 = glMatrix.vec3
 
+/**
+ * Perform a single pass of FABRIK, either in the forward or backward direction
+ * @param {list} Ps List of joints on the arm (update these by reference)
+ * @param {list} ds Distance between adjacent joints on the arm
+ * @param {vec3} p Target position
+ * @param {vec3} p0 Original position of arm anchor
+ * @param {boolean} forward If true, do a forward pass.  If false, do a backward pass
+ */
 function FABRIKInner(Ps, ds, p, p0, forward) {
-    let diff = vec3.create();
-    if (forward) {
-        vec3.copy(Ps[Ps.length-1], p);
-        let i = Ps.length - 2;
-        while (i >= 0) {
-            vec3.subtract(diff, Ps[i], Ps[i+1]);
-            vec3.normalize(diff, diff);
-            vec3.scaleAndAdd(Ps[i], Ps[i+1], diff, ds[i]);
-            i--;
-        }
-    }
-    else {
-        vec3.copy(Ps[0], p0);
-        for (let i = 0; i < Ps.length-1; i++) {
-            vec3.subtract(diff, Ps[i+1], Ps[i]);
-            vec3.normalize(diff, diff);
-            vec3.scaleAndAdd(Ps[i+1], Ps[i], diff, ds[i]);
-        }
-    }
+    // TODO: Fill this in
 }
 
 /**
- * 
+ * Perform a single forward/backward pass of FABRIK
  * @param {list} Ps List of joints on the arm
  * @param {vec3} p Target
  */
